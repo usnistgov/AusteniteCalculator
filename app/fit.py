7da63f85ad7b44b10b93d93bfc93ef18eea06ab1
maxgarman <mgarman@mines.edu>
2022-03-09 21:23:50 -0700

    experimenting with peak verification algorithm WIP

--- app/fit.py
+++ app/fit.py
@@ -89,7 +89,7 @@ def fit_moved_left_peaks(hist, peaks_list, peak_verify):
     hist.refine_peaks()
 
     # Third, fit the area, position, and gaussian componenet of the width
-    hist.set_peakFlags(pos=True,area=True,sig=True)
+    hist.set_peakFlags(pos=True,area=True,sig=True, gam = True)
     hist.refine_peaks()
 
 def fit_moved_right_peaks(hist, peaks_list, peak_verify):
@@ -133,7 +133,7 @@ def fit_moved_right_peaks(hist, peaks_list, peak_verify):
     hist.refine_peaks()
 
     # Third, fit the area, position, and gaussian componenet of the width
-    hist.set_peakFlags(pos=True,area=True,sig=True)
+    hist.set_peakFlags(pos=True,area=True,sig=True, gam = True)
     hist.refine_peaks()   
 
 def fit_peaks_holdsig(hist, peaks_list, Chebyschev_coeffiecients, peak_verify):
@@ -168,7 +168,7 @@ def fit_peaks_holdsig(hist, peaks_list, Chebyschev_coeffiecients, peak_verify):
     #? What to do when signal to noise is poor?  Ways to use good fits to bound parameters for poor fits?
     temp_list = []
     for x in range(peak_verify):
-        if(peak_verify[x]):
+        if(not(peak_verify[x])):
             temp_list.append(peaks_list[x])
     # First fit only the area
     hist.set_peakFlags(area=True)
@@ -214,7 +214,7 @@ def fit_peaks_holdgam(hist, peaks_list, peak_verify):
     #? What to do when signal to noise is poor?  Ways to use good fits to bound parameters for poor fits?
     temp_list = []
     for x in range(peak_verify):
-        if(peak_verify[x]):
+        if(not(peak_verify[x])):
             temp_list.append(peaks_list[x])
 
     # First fit only the area
@@ -291,8 +291,8 @@ def fit_background(DF, hist, peaks_list, sig_width=3):
 def create_verify_list(t_pos, t_int, t_sigma, t_gamma):
     verify_list = np.empty(t_pos.shape[0])
 
-    for x in range(t_pos.shape[0]):
-        if(x < 0):
+    for x in range(t_int.shape[0]):
+        if(t_int[x] < 0):
             verify_list[x] = False
         else:
             verify_list[x] = True
@@ -300,14 +300,16 @@ def create_verify_list(t_pos, t_int, t_sigma, t_gamma):
     m, b = np.polyfit(t_pos, t_sigma, 1)
 
     for x in range(t_pos.shape[0]):
-        if(t_sigma[x] > (m * t_pos[x] + b) + 5 or t_sigma[x] < (m * t_pos[x] + b) - 5):
+        print("\n \n Line of best fit value \n \n")
+        print(t_sigma[x], " = ", m, " * ", t_pos[x], " + ", b)
+        print("\n\n")
+        if(t_sigma[x] > (m * t_pos[x] + b) + t_sigma[1]/2 or t_sigma[x] < (m * t_pos[x] + b) - t_sigma[1]/2):
             verify_list[x] = False
 
     m, b = np.polyfit(t_pos, t_gamma, 1)
 
     for x in range(t_pos.shape[0]):
-        if(t_gamma[x] > (m * t_pos[x] + b) + 5 or t_gamma[x] < (m * t_pos[x] + b) - 5):
+        if(t_gamma[x] > (m * t_pos[x] + b) + t_gamma[1]/2 or t_gamma[x] < (m * t_pos[x] + b) - t_gamma[1]/2):
             verify_list[x] = False
     
-    print(verify_list)
     return verify_list
