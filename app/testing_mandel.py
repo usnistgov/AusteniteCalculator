dcdc9171c570f5a67e7b1fb0c26ff42916da57f5
maxgarman <mgarman@mines.edu>
2022-05-20 11:48:15 -0600

    added more testing for other examples

--- app/testing_mandel.py
+++ app/testing_mandel.py
@@ -82,6 +82,87 @@ def test_EX1():
                     warnings.warn(UserWarning(warning_string))
                 assert abs(check_list.loc[j, keys[i]] - phase_frac_DF.loc[j, keys[i]]) <= check_list.loc[j, keys[i]] * 0.05
     
+def test_EX2():
+    datadir = '../ExampleData/Example02' 
+    cif_fnames = ['austenite-Duplex.cif','ferrite-Duplex.cif']
+    workdir = '../server_workdir'
+    xrdml_fname = 'Gonio_BB-HD-Cu_Gallipix3d[30-120]_New_Control_proper_power.xrdml'
+    instprm_fname = 'TestCalibration.instprm'
+    master_data = '../server_default_datadir/EX2_master.txt'
+    inference_method = 'paul_mandel'
+    check_list, phases = dataGrabber.read_data(master_data)
+    fig1, fig2, results_df, ni_fig, two_theta_fig, phase_frac_DF, uncert_DF, pf_uncertainty_fig, pf_uncertainty_table = compute_results.compute(datadir,workdir,xrdml_fname,instprm_fname,cif_fnames,G2sc,inference_method)
+    
+    keys = check_list.keys()
+    for i in range(len(keys)):
+        for j in range(phases):
+            if(i == 4):
+                continue
+            elif(i == 0 or i == 3):
+                assert check_list.loc[j, keys[i]] == phase_frac_DF.loc[j, keys[i]]
+            else:
+                if(abs(check_list.loc[j, keys[i]] - phase_frac_DF.loc[j, keys[i]]) >= 0.001 and abs(check_list.loc[j, keys[i]] - phase_frac_DF.loc[j, keys[i]]) <= check_list.loc[j, keys[i]] * 0.05):
+                    warning_string = "Values for %s are not exact, but within 5 percent of one another" % keys[i]
+                    warnings.warn(UserWarning(warning_string))
+                elif(abs(check_list.loc[j, keys[i]] - phase_frac_DF.loc[j, keys[i]]) >= check_list.loc[j, keys[i]] * 0.05):
+                    warning_string = "Values for %s have a difference of greater the 5 percent" % keys[i]
+                    warnings.warn(UserWarning(warning_string))
+                assert abs(check_list.loc[j, keys[i]] - phase_frac_DF.loc[j, keys[i]]) <= check_list.loc[j, keys[i]] * 0.05
+    
+def test_EX3():
+    datadir = '../server_default_datadir' 
+    cif_fnames = ['austenite-Duplex.cif','ferrite-Duplex.cif']
+    workdir = '../server_workdir'
+    xrdml_fname = 'Gonio_BB-HD-Cu_Gallipix3d[30-120]_New_Control_proper_power.xrdml'
+    instprm_fname = 'TestCalibration.instprm'
+    master_data = '../server_default_datadir/EX1_master.txt'
+    inference_method = 'paul_mandel'
+    check_list, phases = dataGrabber.read_data(master_data)
+    fig1, fig2, results_df, ni_fig, two_theta_fig, phase_frac_DF, uncert_DF, pf_uncertainty_fig, pf_uncertainty_table = compute_results.compute(datadir,workdir,xrdml_fname,instprm_fname,cif_fnames,G2sc,inference_method)
+    
+    keys = check_list.keys()
+    for i in range(len(keys)):
+        for j in range(phases):
+            if(i == 4):
+                continue
+            elif(i == 0 or i == 3):
+                assert check_list.loc[j, keys[i]] == phase_frac_DF.loc[j, keys[i]]
+            else:
+                if(abs(check_list.loc[j, keys[i]] - phase_frac_DF.loc[j, keys[i]]) >= 0.001 and abs(check_list.loc[j, keys[i]] - phase_frac_DF.loc[j, keys[i]]) <= check_list.loc[j, keys[i]] * 0.05):
+                    warning_string = "Values for %s are not exact, but within 5 percent of one another" % keys[i]
+                    warnings.warn(UserWarning(warning_string))
+                elif(abs(check_list.loc[j, keys[i]] - phase_frac_DF.loc[j, keys[i]]) >= check_list.loc[j, keys[i]] * 0.05):
+                    warning_string = "Values for %s have a difference of greater the 5 percent" % keys[i]
+                    warnings.warn(UserWarning(warning_string))
+                assert abs(check_list.loc[j, keys[i]] - phase_frac_DF.loc[j, keys[i]]) <= check_list.loc[j, keys[i]] * 0.05
+    
+def test_EX4():
+    datadir = '../server_default_datadir' 
+    cif_fnames = ['austenite-Duplex.cif','ferrite-Duplex.cif']
+    workdir = '../server_workdir'
+    xrdml_fname = 'Gonio_BB-HD-Cu_Gallipix3d[30-120]_New_Control_proper_power.xrdml'
+    instprm_fname = 'TestCalibration.instprm'
+    master_data = '../server_default_datadir/EX1_master.txt'
+    inference_method = 'paul_mandel'
+    check_list, phases = dataGrabber.read_data(master_data)
+    fig1, fig2, results_df, ni_fig, two_theta_fig, phase_frac_DF, uncert_DF, pf_uncertainty_fig, pf_uncertainty_table = compute_results.compute(datadir,workdir,xrdml_fname,instprm_fname,cif_fnames,G2sc,inference_method)
+    
+    keys = check_list.keys()
+    for i in range(len(keys)):
+        for j in range(phases):
+            if(i == 4):
+                continue
+            elif(i == 0 or i == 3):
+                assert check_list.loc[j, keys[i]] == phase_frac_DF.loc[j, keys[i]]
+            else:
+                if(abs(check_list.loc[j, keys[i]] - phase_frac_DF.loc[j, keys[i]]) >= 0.001 and abs(check_list.loc[j, keys[i]] - phase_frac_DF.loc[j, keys[i]]) <= check_list.loc[j, keys[i]] * 0.05):
+                    warning_string = "Values for %s are not exact, but within 5 percent of one another" % keys[i]
+                    warnings.warn(UserWarning(warning_string))
+                elif(abs(check_list.loc[j, keys[i]] - phase_frac_DF.loc[j, keys[i]]) >= check_list.loc[j, keys[i]] * 0.05):
+                    warning_string = "Values for %s have a difference of greater the 5 percent" % keys[i]
+                    warnings.warn(UserWarning(warning_string))
+                assert abs(check_list.loc[j, keys[i]] - phase_frac_DF.loc[j, keys[i]]) <= check_list.loc[j, keys[i]] * 0.05
+    
 
 def test_EX5():
     datadir = '../ExampleData/Example05'
