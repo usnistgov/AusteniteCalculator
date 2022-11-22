5a9347e87ba8bed41327f993861111fa9cd62b76
dtn1 <david.newton@nist.gov>
2022-11-22 13:35:56 -0700

    rebuild container on AWS from script

---
+++ redeploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+rm AusteniteCalculator-master.zip
+rm -rf AusteniteCalculator-master
+
+wget https://github.com/usnistgov/AusteniteCalculator/archive/refs/heads/master.zip
+unzip master.zip
+
+sudo docker build -t ac ./AusteniteCalculator-master
+sudo docker rm -f $(sudo docker ps -a -q)
+sudo docker image prune -f
+sudo docker run -d -p 80:8050 ac
